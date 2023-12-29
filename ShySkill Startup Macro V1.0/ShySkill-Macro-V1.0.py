0b72f0759028d4799d82ea0421591c0b7a213f10
ShySkill <117660637+ShySkill@users.noreply.github.com>
2023-12-28 21:28:01 -0500

    Add files via upload

---
+++ ShySkill Startup Macro V1.0/ShySkill-Macro-V1.0.py
@@ -0,0 +1,64 @@
+import time, keyboard, webbrowser, pyautogui, subprocess, os
+
+#starts up roblox and claims the hive
+def startUpRoblox():
+    #opens roblox and waits for it to load (30 seconds for 2.50ghz+, 60 seconds for )
+    webbrowser.open_new('https://www.roblox.com/share?code=203f00aa80eea841ab5e96367e3d86a3&type=Server') 
+    time.sleep(30)
+
+    #roblox has finished loading and you are now waiting at spawn
+
+    #closes the browser to minimize CPU usage
+    keyboard.press("alt+tab")
+    time.sleep(0.2)
+    keyboard.release("alt+tab")
+    time.sleep(0.8)
+    keyboard.press("alt+F4")
+    time.sleep(0.2)
+    keyboard.release("alt+F4")
+    #runs to the center hive and claims it
+    time.sleep(1)
+    pressHoldKey("w", 2)
+    keyboard.press("e")
+
+
+def startUpNatro():
+    try:
+        natro_path = r'C:\Users\kesoc\Downloads\Natro_Macro_v0.9.2\Natro Macro v0.9.2'
+        os.system(f'cmd /c "cd {natro_path} && Natro_macro.ahk"')
+        time.sleep(10)
+        keyboard.press("F1")
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error Running the Natro Macro. Shutting down and trying again.")
+
+#main press and hold
+def pressHoldKey(key_to_hold, hold_duration):
+
+    start_time = time.time()
+    while time.time() - start_time < hold_duration:
+        phk(key_to_hold, 0.1)  
+
+    keyboard.release(key_to_hold)
+
+#@param key and duration are passed here
+#press and hold the key
+def phk(key, duration):
+    keyboard.press(key)
+    time.sleep(duration)
+    keyboard.release(key)
+
+def loadWindowsAndWait():
+    print("Python File Executed")
+
+    for i in range(0, 10):
+        time.sleep(1)
+        print("Countdown: " + str(i))
+
+def main():
+    startUpRoblox()
+    startUpNatro()
+
+if __name__ == "__main__":
+    main()
+    quit()
